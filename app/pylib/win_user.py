d0ad91632a49d982080bb5732a89ad2666850dc7
Scadic <46495721+Scadic@users.noreply.github.com>
2020-01-23 09:21:33 +0100

    Update win_user.py
    Now using re to compile reg. exp. Also using 'cn' to get ou.

--- app/pylib/win_user.py
+++ app/pylib/win_user.py
@@ -2,6 +2,7 @@ import pywin32_system32
 import win32api
 from pyad import pyad, aduser, adobject, adgroup, addomain, adcontainer, adcomputer, adquery, adsearch
 from ionracing import __init__
+import re
 
 ldap_server = "<server_fqdn>" # Fully qualified domain name of the ldap server
 username = "<accountop_username>" # Account operations account
@@ -16,7 +17,7 @@ company = "<your_companyname>" # Official Company name
 userdomain = "<yourdomain>" # Just your domain name, ie. google, not google.com
 domainsuffix = "<suffix>" # What top level domain you have, ie. .com, .net, .eu, dk, .no, .se
 ous = [('<ou_name>', '<Ou_name>'), ('<ou_name2>', '<Ou_name2>')] # Organizational units list for the wtf form <type:list> of <type:tuple> or <type:str> and <type:str>
-ou_regex = "^((?!<ou_name>)(?!<ou_name2>)).*$"# OU regex to match anything else than the ou names
+ou_regex = re.compile()"^((?!<ou_name>)(?!<ou_name2>)).*$")# OU regex to match anything else than the ou names
 
 def create_user_settings(user_input):
     '''
@@ -69,7 +70,7 @@ def create_user(user_settings, password, q):
         )
     #ou_arg = q.get_single_result().get("distinguishedName")
     pyad.set_defaults(ldap_server=ldap_server, username=username, password=password)
-    ou = adcontainer.ADContainer.from_dn(q.get_single_result().get("distinguishedName"))
+    ou = adcontainer.ADContainer.from_cn(q.get_single_result().get("cn"))
     name = user_settings["name"]
     user = aduser.ADUser.create(
         name=name,
